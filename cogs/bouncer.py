128799a20b46f5160b22ed4bdb98ef3ccd4a4be3
Paweł Wyroślak <109243554+BosaKaczka@users.noreply.github.com>
2023-05-08 22:38:23 +0200

    Kuba joins
    finally

---
+++ cogs/bouncer.py
@@ -0,0 +1,26 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+class BOUNCER(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Bouncer ready')
+
+    @app_commands.command(name="kamikaze", description='Try me!')
+    async def kamikaze(self, interaction: discord.Interaction) -> None:
+        if interaction.user.voice and interaction.user.voice.channel:
+            channel = interaction.user.voice.channel
+            await channel.connect()
+            members = interaction.user.voice.channel.members
+            for member in members:
+                await member.move_to(None)
+
+
+
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(BOUNCER(bot), guilds=[discord.Object(id=690938920199782420)])
--- cogs/bugs.py
+++ cogs/bugs.py
@@ -15,7 +15,7 @@ class BUGS(commands.Cog):
     async def bug_report(self, interaction: discord.Interaction, bug_description: str) -> None:
         embed = discord.Embed(title="Report has been sent")
         embed.add_field(name=f"{str(interaction.user)[:-5]} reported a bug", value=f"**Bug description**:\n{bug_description}", inline=False)
-        f = open("./feedback/bugs.txt", "a")
+        f = open("./feedback/bugs.txt", "a", encoding="ISO-8859-2")
         f.write(f"{str(interaction.user)}\n{bug_description}\n\n")
         f.close()
         await interaction.response.send_message(embed=embed, ephemeral=True)
